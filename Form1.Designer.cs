5603c2ef4a70049380ff13e6264d25b3fbd471b1
andreywin2022 <115880247+andreywin2022@users.noreply.github.com>
2022-10-15 13:15:07 +0000

    Add files via upload
    Air siren Ukraine

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ALERT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 223);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ALERT";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+namespace ALERT 
+{
+    public partial class Form1 : Form   
+    {
+        bool b = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ahtung();
+            timer1.Interval = 60000;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ahtung();
+        }
+
+        private async void ahtung ()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                //https----------------------------------------
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                var json = await httpClient.GetStringAsync("https://alarmmap.online/assets/json/_alarms/siren.json");
+                //---------------------------------------------
+                List<Item> items;
+                items = JsonConvert.DeserializeObject<List<Item>>(json);
+                int i;
+                for (i = 0; i <= items.Count - 1; i++)
+                {
+                    var tmp = items[i];
+                    string tmp_dist = tmp.district;
+                    // string tmp_time = tmp.start;
+                    //-------------------------------------------------------------
+                    //if (tmp_dist == "Київська_область")
+                    //if (tmp_dist == "Луганська_область")
+                    if (tmp_dist == "м_Київ")
+                  
+                    {
+                        pictureBox1.Image = Image.FromFile("ahtung.jpg");
+                   
+                        this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+                        //-----media
+                        if (b == true)
+                        {
+                            WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+                            player.URL = "alert.mp3";
+                            player.controls.play();
+                            b = false;
+                            break;
+                        }
+                        //----------
+                        return;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = Image.FromFile("noahtung.jpg");
+                        b = true;
+                        this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+                    }
+                }
+            }
+        }
+    }
+    public class Item
+    {
+        public string district;
+        public string start;
+    }
+
+}
